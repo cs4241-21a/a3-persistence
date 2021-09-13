953ba49db018b220d2330fca5b4df8485fcf82c5
theaniartist <wsly@wpi.edu>
2021-09-13 16:23:22 -0400

    Add mongoDB

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
-const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const express = require( 'express' ),
+      mongodb = require( 'mongodb' ),
+      app = express()
+
+app.use( express.static('public') )
+app.use( express.json() )
+
+// make sure to substitute your username / password for tester:tester123 below!!! 
+const uri = 'mongodb+srv://tester:helloWorld@cluster0.f9zkh.mongodb.net/'
+
+const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
+let collection = null
+
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db( 'datatest' ).collection( 'test' )
+  })
+  .then( __collection => {
+    // store reference to collection
+    collection = __collection
+    // blank query returns all documents
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
+  
+// route to get all docs
+app.get( '/', (req,res) => {
+  if( collection !== null ) {
+    debugger
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
+})
+
+app.post( '/add', (req,res) => {
+  // assumes only one object to insert
+  collection.insertOne( req.body ).then( result => res.json( result ) )
+})
 
 app.use(function(req, res, next) {
     console.log(req.url)
     next()
 })
-
-app.use(express.static('./'))
-
 
 app.post("/add", bodyParser.json(),  (request, response) => {
     console.log( request.body)
