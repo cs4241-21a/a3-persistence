849d2478564a8dd1c86537b7880980a7b244643b
theaniartist <wsly@wpi.edu>
2021-09-14 16:03:15 -0400

    Add some server code

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require( 'express' ),
+      mongodb = require( 'mongodb' ),
+      dotenv = require('dotenv').config(),
+      app = express();
+
+app.use( express.static('public') )
+app.use( express.json() )
+
+const uri = 'mongodb+srv://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST
+
+const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
+let collection = null
+
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db( 'XXXtest' ).collection( 'XXXtodos' )
+  })
+  .then( __collection => {
+    // store reference to collection
+    collection = __collection
+    // blank query returns all documents
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
+  
+// route to get all docs
+app.get( '/', (req,res) => {
+  if( collection !== null ) {
+    // get array and pass to res.json
+    collection.find({ }).toArray().then( result => res.json( result ) )
+  }
+})
+
+//Add a route to insert a todo
+app.post( '/add', (req,res) => {
+    // assumes only one object to insert
+    collection.insertOne( req.body ).then( result => res.json( result ) )
+})
+
+//Add a route to remove a todo
+app.post( '/remove', (req,res) => {
+    collection
+      .deleteOne({ _id:mongodb.ObjectId( req.body._id ) })
+      .then( result => res.json( result ) )
+})
+
+//Add a route to update a document
+app.post( '/update', (req,res) => {
+    collection
+      .updateOne(
+        { _id:mongodb.ObjectId( req.body._id ) },
+        { $set:{ name:req.body.name } }
+      )
+      .then( result => res.json( result ) )
+  })
+  
+app.listen( 3000 )
