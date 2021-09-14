2e7a60ea33053b06d0ab6b301518bee2c1d48718
God <Matthew7758@users.noreply.github.com>
2021-09-14 15:27:48 -0400

    Changed listen port to be compatible with Heroku and other hosting services. TODO: Modify views to make things pretty. Use CSS as per assignment specs.

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.get( '/', function (req, res) {
     res.sendFile(__dirname + "/public/login/login.html" )
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening at http://localhost:${3000}`)
 })
 
