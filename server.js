c127ada9a74facf0fd94bf6baca29fc25f78dc73
Benstaw <ben@2broadview.com>
2021-09-17 16:07:39 -0400

    small oops

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ let appdata = [
 ]
 
 const MongoClient = mongodb.MongoClient;
-const uri = 'mongodb+srv://admin:admin@cluster0.kt8ex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:true });
 let collection = null
 
