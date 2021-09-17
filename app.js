42e2e535bc0139b1da9fe261ca1f6ce190430685
Timothy Goon <t.kgoonx@gmail.com>
2021-09-17 19:40:48 -0400

    added a favicon.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon')
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
