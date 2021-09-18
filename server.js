cf9d2bf81ba203c19a00851b38d9ad50676fa8cd
GTWFam <252704436go@gmail.com>
2021-09-18 17:12:12 -0400

    initial login logic

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cookie = require('cookie-session');
 const mongodb = require('mongodb');
 const app = express();
 
-app.use(function (req, res, next) {
-    next();
-});
+app.use(cookie({
+    name: 'session',
+    keys: ['key1', 'key2']
+}));
 
 app.use(express.static('views'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 const uri = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST;
 
@@ -21,14 +24,40 @@ client.connect()
     })
     .then(__collection => {
         collection = __collection;
-        return collection.find({ "username": "Eri" }).toArray()
     })
-    .then(console.log)
 
 app.get('/', (req, res) => {
     if (collection !== null) {
-        collection.find({}).toArray.then(result => res.json(result))
+        collection.find({}).toArray().then(result => res.json(result))
     }
-})
+});
+
+app.post('/login', async function (req, res) {
+    let reqUser = req.body.username;
+    let reqPass = req.body.password;
+    let userList = await collection.find({
+        "username": reqUser,
+        "password": reqPass
+    }).toArray();
+
+    console.log(userList);
+
+    if (userList.length === 1) {
+        req.session.login = true;
+        res.redirect('main.html');
+    } else {
+        res.sendFile(__dirname + '/views/index.html');
+    }
+});
+
+app.use(function (req, res, next) {
+    if (req.session.login === true) {
+        next();
+    } else {
+        res.sendFile(__dirname + '/views/index.html');
+    }
+});
+
+app.use(express.static('views'));
 
 app.listen(3000);
