8b96b35ce4db8ecd1f47442a896fc1424ded71cd
Michael Zeolla <62816869+Mikeythunder@users.noreply.github.com>
2021-09-19 13:36:55 -0400

    updates

--- env.js
+++
@@ -1,1 +0,0 @@
-const DatabasePW = 'Michael1'
--- server.improved.js
+++ server.improved.js
@@ -169,7 +169,8 @@ app.post('/update', bodyParser.json(), function(request, response) {
 
 
 
-const listener = app.listen(3000, () => {
+const listener = 
+app.listen(process.env.PORT || 5000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
 
