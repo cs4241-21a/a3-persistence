97ff8cd3e744991ec0ec0be42898c3f030fa4472
GTWFam <252704436go@gmail.com>
2021-09-19 00:41:38 -0400

    Auth with passport js

--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const cookie = require('cookie-session');
-const mongodb = require('mongodb');
 const app = express();
-
-app.use(cookie({
-    name: 'session',
-    keys: ['key1', 'key2']
-}));
+const mongodb = require('mongodb');
+const flash = require('express-flash');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy
 
 app.use(express.static('views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 const uri = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST;
 
@@ -32,32 +38,40 @@ app.get('/', (req, res) => {
     }
 });
 
-app.post('/login', async function (req, res) {
-    let reqUser = req.body.username;
-    let reqPass = req.body.password;
-    let userList = await collection.find({
-        "username": reqUser,
-        "password": reqPass
-    }).toArray();
-
-    console.log(userList);
-
-    if (userList.length === 1) {
-        req.session.login = true;
-        res.redirect('main.html');
-    } else {
-        res.sendFile(__dirname + '/views/index.html');
+const authUser = async (username, password, next) => {
+    let user = await collection.find({ "username": username }).toArray();
+    if (user === []) {
+        return next(null, false, { message: 'No user with that username!' });
     }
+
+    if (password === user[0].password) {
+        return next(null, user[0])
+    } else {
+        return next(null, false, { message: 'Incorrect password!' })
+    }
+
+}
+
+let getUserbyId = async id => {
+    if (collection !== null) {
+        return collection.find({ "_id": id }).toArray()[0];
+    } else {
+        return null
+    }
+}
+
+passport.use(new LocalStrategy({}, authUser));
+passport.serializeUser((user, next) => next(null, user._id))
+passport.deserializeUser((id, next) => {
+    next(null,);
 });
 
-app.use(function (req, res, next) {
-    if (req.session.login === true) {
-        next();
-    } else {
-        res.sendFile(__dirname + '/views/index.html');
+app.post('/login', passport.authenticate('local',
+    {
+        successRedirect: 'main.html',
+        failureRedirect: '/',
+        failureFlash: true
     }
-});
-
-app.use(express.static('views'));
+));
 
 app.listen(3000);
