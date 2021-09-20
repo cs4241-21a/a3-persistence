5684f659f56c02cdb5aecad76ac163e5d9b59093
ajwhitney23 <ajwhitney@wpi.edu>
2021-09-20 12:02:15 -0400

    updated for final push

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const { query, response } = require('express'); //might not need
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
 const express = require( 'express' );
@@ -65,7 +64,6 @@ app.get( '/logout', (req, res) => {
 app.use( express.static('public') )
 app.use( express.json() )
 
-// make sure to substitute your username / password for tester:tester123 below!!! 
 const uri = 'mongodb+srv://mydbuser:sd4A47HHLOuJ7rQJ@cluster0.yb8hn.mongodb.net/'
 
 const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
@@ -117,107 +115,3 @@ app.post( '/remove', (req,res) => {
 
   
 app.listen( process.env.PORT || 3000 )
-
-// const http = require("http"),
-//   fs = require("fs"),
-//   // IMPORTANT: you must run `npm install` in the directory for this assignment
-//   // to install the mime library used in the following line of code
-//   mime = require("mime"),
-//   dir = "public/",
-//   port = 3000;
-
-// /* default data is empty */
-// const appdata = [];
-
-// const server = http.createServer(function (request, response) {
-//   if (request.method === "GET") {
-//     handleGet(request, response);
-//   } else if (request.method === "POST") {
-//     handlePost(request, response);
-//   }
-// });
-
-// const handleGet = function (request, response) {
-//   const filename = dir + request.url.slice(1);
-
-//   if (request.url === "/") {
-//     sendFile(response, "public/index.html");
-//   } else {
-//     sendFile(response, filename);
-//   }
-// };
-
-// /* interact with sumbit and remove requests, able to interact with existing data
-// by matching the name and team */
-// const handlePost = function (request, response) {
-//   let dataString = "";
-
-//   request.on("data", function (data) {
-//     dataString += data;
-//   });
-
-//   /* check if the request is adding or removing data */
-//   if (request.url === "/submit") {
-//     request.on("end", function () {
-//       const dataJSON = JSON.parse(dataString);
-//       let avg = dataJSON.time / dataJSON.laps;
-//       dataJSON.avg = avg;
-//       let modified = false;
-//       for (let i = 0; i < appdata.length; i++) {
-//         if (
-//           appdata[i].name.toUpperCase() === dataJSON.name.toUpperCase() &&
-//           appdata[i].team.toUpperCase() === dataJSON.team.toUpperCase()
-//         ) {
-//           appdata[i] = dataJSON;
-//           modified = true;
-//         }
-//       }
-//       if (!modified) {
-//         appdata.push(dataJSON);
-//       }
-//       /* sorting data by fastest average time */
-//       appdata.sort(function (a, b) {
-//         return a["avg"] - b["avg"];
-//       });
-//       response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-//       response.end(JSON.stringify(appdata));
-//     });
-//   } else if (request.url === "/remove") {
-//     request.on("end", function () {
-//       const dataJSON = JSON.parse(dataString);
-//       for (let i = 0; i < appdata.length; i++) {
-//         if (
-//           appdata[i].name.toUpperCase() === dataJSON.name.toUpperCase() &&
-//           appdata[i].team.toUpperCase() === dataJSON.team.toUpperCase()
-//         ) {
-//           appdata.splice(i, 1)
-//         }
-//       }
-//       /* sorting data by fastest average time */
-//       appdata.sort(function (a, b) {
-//         return a["avg"] - b["avg"];
-//       });
-//       response.writeHead(200, "OK", { "Content-Type": "text/plain" });
-//       response.end(JSON.stringify(appdata));
-//     });
-//   }
-// };
-
-// const sendFile = function (response, filename) {
-//   const type = mime.getType(filename);
-
-//   fs.readFile(filename, function (err, content) {
-//     // if the error = null, then we've loaded the file successfully
-//     if (err === null) {
-//       // status code: https://httpstatuses.com
-//       response.writeHeader(200, { "Content-Type": type });
-//       response.end(content);
-//     } else {
-//       // file not found, error code 404
-//       response.writeHeader(404);
-//       response.end("404 Error: File Not Found");
-//     }
-//   });
-// };
-
-// server.listen(process.env.PORT || port);
