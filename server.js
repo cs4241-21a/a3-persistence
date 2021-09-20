637e9b89681cd87b1e390eace0b96a221de2ef41
Benstaw <ben@2broadview.com>
2021-09-20 15:46:38 -0400

    partial auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { count } = require('console')
 
 const http = require( 'http' ),
+      cookie  = require( 'cookie-session' ),
       fs   = require( 'fs' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
       // to install the mime library used in the following line of code
@@ -17,14 +18,23 @@ const bodyparser = require('body-parser');
 app.use(express.static("public"));
 app.use(express.json());
 
+app.use( express.urlencoded({ extended:true }) )
+
+
+
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+  response.sendFile(__dirname + "/views/login.html");
 })
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 })
+
+app.use( cookie({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 
 const MongoClient = mongodb.MongoClient;
 const uri =`mongodb+srv://${user}:${pass}@cluster0.kt8ex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -41,8 +51,42 @@ client.connect()
 })
 //.then(console.log)
 
+
+app.post( '/login', function(req,res) {
+  // express.urlencoded will put your key value pairs 
+  // into an object, where the key is the name of each
+  // form field and the value is whatever the user entered
+  console.log( req.body )
+  
+  // below is *just a simple authentication example* 
+  // for A3, you should check username / password combos in your database
+  if( req.body.password === 'test' ) {
+    // define a variable that we can check in other middleware
+    // the session object is added to our requests by the cookie-session middleware
+    req.session.login = true
+
+    // https://stackoverflow.com/questions/10827242/understanding-the-post-redirect-get-pattern 
+    // make redirect
+    res.sendFile( __dirname + '/views/index.html' )
+      .then(dbresponse => {
+        return collection.find({'_id':dbresponse.insertedId}).toArray()
+      })
+  }else{
+    // password incorrect, redirect back to login page
+    res.sendFile( __dirname + '/views/login.html' )
+  }
+})
+
+// add some middleware that always sends unauthenicaetd users to the login page
+app.use( function( req,res,next) {
+  if( req.session.login === true )
+    next()
+  else
+    res.sendFile( __dirname + '/views/login.html' )
+})
+
 app.post('/submit', bodyparser.json(),function(req, res){
-  //console.log('body:', req.body)
+ // console.log('aaaaaaa', req.body)
   collection.insertOne(req.body)
   .then(dbresponse => {
     return collection.find({'_id':dbresponse.insertedId}).toArray()
