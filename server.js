2780c739e40abf13df6a6bfc4069827bb061fd03
GTWFam <252704436go@gmail.com>
2021-09-20 00:08:04 -0400

    Login & Register logic

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const app = express();
 const mongodb = require('mongodb');
 const flash = require('express-flash');
 const session = require('express-session');
+const methodOverride = require('method-override');
+
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy
 
-app.use(express.static('views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
@@ -18,6 +19,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOverride('_method'))
 
 const uri = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST;
 
@@ -30,22 +32,30 @@ client.connect()
     })
     .then(__collection => {
         collection = __collection;
-    })
+        return collection.find({ "username": "test" }).toArray()
+    }).then(console.log)
 
-app.get('/', (req, res) => {
-    if (collection !== null) {
-        collection.find({}).toArray().then(result => res.json(result))
-    }
+app.get('/', checkNotAuthenticated, (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/main', checkAuthenticated, (req, res) => {
+    res.sendFile(__dirname + '/views/main.html');
+});
+
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.sendFile(__dirname + '/views/register.html');
+})
 
 const authUser = async (username, password, next) => {
     let user = await collection.find({ "username": username }).toArray();
-    if (user === []) {
+    user = user[0];
+    if (user === undefined) {
         return next(null, false, { message: 'No user with that username!' });
     }
 
-    if (password === user[0].password) {
-        return next(null, user[0])
+    if (password === user.password) {
+        return next(null, user)
     } else {
         return next(null, false, { message: 'Incorrect password!' })
     }
@@ -54,7 +64,7 @@ const authUser = async (username, password, next) => {
 
 let getUserbyId = async id => {
     if (collection !== null) {
-        return collection.find({ "_id": id }).toArray()[0];
+        return await collection.find({ "_id": id }).toArray()[0];
     } else {
         return null
     }
@@ -63,15 +73,51 @@ let getUserbyId = async id => {
 passport.use(new LocalStrategy({}, authUser));
 passport.serializeUser((user, next) => next(null, user._id))
 passport.deserializeUser((id, next) => {
-    next(null,);
+    next(null, getUserbyId(id));
 });
 
-app.post('/login', passport.authenticate('local',
+app.post('/login', checkNotAuthenticated, passport.authenticate('local',
     {
-        successRedirect: 'main.html',
+        successRedirect: '/main',
         failureRedirect: '/',
         failureFlash: true
     }
 ));
+
+app.delete('/logout', (req, res) => {
+    req.logOut();
+    res.redirect('/');
+});
+
+app.post('/registerUser', (req, res) => {
+    if (collection === null) {
+        alert('Failed to connect to the databse!');
+        return res.redirect('/register');
+    }
+    try {
+        collection.insertOne({
+            "username": req.body.username,
+            "password": req.body.password
+        });
+        return res.redirect('/');
+    } catch (e) {
+        alert('Failed to register the user: ' + e.message);
+        return res.redirect('/register');
+    }
+});
+
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/');
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/main');
+    }
+    next()
+}
 
 app.listen(3000);
