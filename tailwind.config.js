dea564c32e94e948894ebfc99962ea0733a9578c
G3P <73323107+GP2P@users.noreply.github.com>
2021-09-23 00:41:30 +0800

    Custom designed color palette and refactoring

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     enabled: true,
@@ -8,13 +10,37 @@ module.exports = {
   },
   darkMode: false,
   theme: {
-    extend: {},
+	extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+      myGreen: {
+        400:'#63D397',
+        500:'#51BC83',
+        600:'#3EB575',
+        700:'#359A63',
+        800:'#2b8656',
+        900:'#246A45',
+      },
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [
     require('tailwindcss'),
-    require('autoprefixer')
+    require('autoprefixer'),
+    require('@tailwindcss/forms')
   ],
 }
