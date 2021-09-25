4ec75064939d6e59085d0bac1602a63f04b0a78f
jcscheufele <57052259+jcscheufele@users.noreply.github.com>
2021-09-25 01:54:17 -0400

    added soundtracks and effects, fixed game edge cases

--- public/script.js
+++ public/script.js
@@ -4,19 +4,50 @@ const logout = function () {
     fetch("/logout")
 }
 
+playMenu = new Audio('/public/menu_track.mp3')
+playCorrect = new Audio('/public/correct.mp3')
+playWrong = new Audio("/public/trump_wrong.mp3")
+
 let time = 60
 let timer = null
 let correct = 0
 let clock = null
 let started = false
 let mode = null
+let flipper = null
 
 const endGame = function () {
     started = false
     clearInterval(timer)
     document.removeEventListener("keydown", handleKeys)
+    document.getElementById("answer").value = "Answer"
+    ans = ""
     document.getElementById("bar").value = 60
     document.getElementById("bar").className = "nes-progress is-pattern"
+    const multiply = document.getElementById("multiply")
+    document.getElementById("multiply").className = "nes-btn"
+    multiply.onclick = function() {
+        mode = 0
+        document.getElementById("start").className = "nes-btn is-primary"
+        document.getElementById("mode").innerText = "Multiply"
+    }
+    const add = document.getElementById("add")
+    document.getElementById("add").className = "nes-btn"
+    add.onclick = function() {
+        mode = 1
+        document.getElementById("start").className = "nes-btn is-primary"
+        document.getElementById("mode").innerText = "Add"
+    }
+    let flip = false
+    flipper = setInterval(() => {
+        if (flip) {
+            document.getElementById("running_correct").className = "nes-text is-success"
+            flip = false
+        } else {
+            document.getElementById("running_correct").className = "nes-text"
+            flip = true
+        }
+    }, 250);
     pressButtons("is-success")
 }
 
@@ -45,7 +76,23 @@ const tick = function () {
 const start = function () {
     if (!started) {
         started = true
+        correct = 0
+        time = 60
+        history = []
+        displayHistory()
+        clearInterval(flipper)
+        playMenu.play()
         document.getElementById("bar").value = time
+        document.getElementById("running_correct").innerText = "0"
+        const mulitply = document.getElementById("multiply")
+        document.getElementById("multiply").className = "nes-btn is-disabled"
+        mulitply.onclick = null
+        const add = document.getElementById("add")
+        document.getElementById("add").className = "nes-btn is-disabled"
+        add.onclick = null
+        document.getElementById("running_correct").className = "nes-text"
+        document.getElementById("bar").className = "nes-progress is-success"
+        document.getElementById("question").className = "nes-text is-primary"
         timer = setInterval(tick,1000)
         document.addEventListener("keydown", handleKeys)
         makeQuiz()
@@ -59,9 +106,15 @@ let question = ""
 let history = []
 
 const makeQuiz = function () {
-    X = Math.floor(Math.random() * 100)
-    Y = Math.floor(Math.random() * 100)
-    question = X + " + " + Y
+    if (mode) {
+        X = Math.floor(Math.random() * 100)
+        Y = Math.floor(Math.random() * 100)
+        question = X + " + " + Y
+    } else {
+        X = Math.floor(Math.random() * 12)
+        Y = Math.floor(Math.random() * 12)
+        question = X + " x " + Y
+    }
     document.getElementById("question").innerText = question
 }
 
@@ -84,7 +137,8 @@ const handleKeys = function ( e ) {
         }
     } else if (e.keyCode === 13) {
         answer = document.getElementById("answer").innerText
-        if (answer == (X+Y)) {
+        if ((answer == (X+Y)) && (mode===1) || (answer == (X*Y)) && (mode===0)) {
+            playCorrect.play()
             correct += 1
             history.push(question + " = " + ans)
             displayHistory()
@@ -93,6 +147,7 @@ const handleKeys = function ( e ) {
             ans = ""
             makeQuiz()
         } else {
+            playWrong.play()
             document.getElementById("answer").innerText = "Nope"
             ans = ""
         }
@@ -101,23 +156,26 @@ const handleKeys = function ( e ) {
 
 
 window.onload = function() {
-    //const createUser_button = document.querySelector("#createUser")
-    //createUser_button.onclick = createUser
+    document.getElementById("start").className = "nes-btn is-disabled"
+
     const logout_button = document.getElementById("logout")
     logout_button.onclick = logout
 
     const start_button = document.getElementById("start")
-    start_button.onclick = start
 
     const multiply = document.getElementById("multiply")
     multiply.onclick = function() {
         mode = 0
+        document.getElementById("start").className = "nes-btn is-primary"
+        start_button.onclick = start
         document.getElementById("mode").innerText = "Multiply"
     }
 
     const add = document.getElementById("add")
     add.onclick = function() {
         mode = 1
+        document.getElementById("start").className = "nes-btn is-primary"
+        start_button.onclick = start
         document.getElementById("mode").innerText = "Add"
     }
 }
