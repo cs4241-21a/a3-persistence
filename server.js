bf48303c85a8701253f000163ed89561610cbeef
AzinxtheOnix <mzlai@wpi.edu>
2021-09-26 04:15:56 -0400

    Highscore update - a2 complete

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+let nextID = 4; // If you have default ID's being set, make sure this one gets set to an available ID 
+
 const http = require( 'http' ),
       fs   = require( 'fs' ),
       // IMPORTANT: you must run `npm install` in the directory for this assignment
@@ -21,9 +23,25 @@ const server = http.createServer( function( request,response ) {
   }
 });
 
+// Takes a specific game and returns the position of the highscore for that game
+// Returns -1 if that game doesn't exist or if no highscore exists
+function findHighscore(game) {
+  let hsCounter = -1;
+  let hsScoreCounter = -1;
+  
+  for(let i = 0; i < appdata.length; i++) {
+    if(appdata[i].game === game && appdata[i].score > hsScoreCounter) {
+      hsScoreCounter = appdata[i].score;
+      hsCounter = i;
+    }
+  }
+
+  return hsCounter;
+}
+
 // Handle GET requests (don't touch this you fool, you moron)
 const handleGet = function( request, response ) {
-  console.log(request.url);
+  console.log("GET Request URL: %s", request.url);
   const filename = dir + request.url.slice( 1 );
 
   if( request.url === '/' ) {
@@ -55,65 +73,115 @@ const handlePost = function( request, response ) {
 
     // Take the string and create a JSON object
     const jsonInput = JSON.parse(dataString);
-    console.log("POST Request Data:");
-    console.log(jsonInput);
 
     if(request.url === '/delete') { // Delete request
       console.log("POST Request: Delete");
+      console.log("POST Request Data:");
+      console.log(jsonInput);
 
       // Only item in this JSON object is the ID; that's all we need
       for(let i = 0; i < appdata.length; i++) {
         if(appdata[i].id === jsonInput.id) { // ID Matches
+          let hsStatus = appdata[i].highscore;
+          let hsGame = appdata[i].game;
           appdata.splice(i, 1);
-          console.log("Deletion complete");
-          break;
+          console.log("Deletion Complete");
+
+          if(hsStatus === true) { // We then need to find a new highscore
+            let hsNew = findHighscore(hsGame); // Find the high score for a specific game
+            if(hsNew !== -1) {
+              appdata[hsNew].highscore = true;
+            }
+          }
+
+          break; // Stop search
         }
       }
-      /*if(jsonInput.highscore === true) { // Highscore Update
-        let high = -1;
-        let score = 0;
-        for(let i = 0; i < appdata.length; i++) {
-          if(appdata[i].game === jsonInput.game && appdata[i].score > score) {
-            high = i;
-            score = appdata[i].score;
-          }
-        }
-        if(high !== -1) { // New Highscore
-          appdata[high].highscore = true;
-        }
-      }*/
 
     } else if(request.url === '/submit') { // Handle modify case as well
       console.log("POST Request: Submit");
+      console.log("POST Request Data:");
+      console.log(jsonInput);
 
       if(jsonInput.id === -1) { // New submission
+        // Set the ID
+        jsonInput.id = nextID;
+        nextID++;
+
+        // Find if there is a highscore to replace
+        let findHS = findHighscore(jsonInput.game);
+        if(findHS !== -1) { // Found highscore, time to see if it's to be replaced
+          if(jsonInput.score > appdata[findHS].score) { // Replace highscore
+            appdata[findHS].highscore = false;
+            jsonInput.highscore = true;
+          }
+        } else { // Did not find highscore, it's now the highscore
+          jsonInput.highscore = true;
+        }
+
         appdata.push(jsonInput); // Add the json info into appdata
-        console.log("Submission complete");
+        console.log("Submission Complete");
+
       } else { // Must be a modification
         for(let i = 0; i < appdata.length; i++) {
           if(appdata[i].id === jsonInput.id) { // ID Matches
-            appdata.splice(i, 1, jsonInput);
-            console.log("Modification complete");
-            break;
+            
+            appdata[i].name = jsonInput.name; // First, set the name
+
+            // First, check if there's a game change
+            if(appdata[i].game === jsonInput.game) { // Same game
+
+              if(appdata[i].highscore === true) { // If it was the highscore
+                appdata[i].score = jsonInput.score;
+                let findHS = findHighscore(jsonInput.game);
+                if(findHS !== i) {
+                  // The highscore is now a different entry
+                  appdata[i].highscore = false;
+                  appdata[findHS].highscore = true;
+                }
+                
+              } else if(appdata[i].highscore === false) { // If it wasn't the highscore
+                let findHS = findHighscore(jsonInput.game);
+                appdata[i].score = jsonInput.score;
+                if (appdata[findHS].score < jsonInput.score) {
+                  // It is the highscore now
+                  appdata[i].highscore = true;
+                  appdata[findHS].highscore = false;
+                }
+              }
+
+            } else { // Different game
+              let previousGame = appdata[i].game; // Save the previous game
+              let newGameCurrentHS = findHighscore(jsonInput.game); // Grab the highscore for the game that we're switching to
+              
+              appdata[i].game = jsonInput.game; // Change game in appdata
+
+              // First, update the highscore for the previous game
+              let previousGameHS = findHighscore(previousGame);
+              if(previousGameHS !== -1) { // If there is a new highscore
+                appdata[previousGameHS].highscore = true;
+              }
+
+              // Then, update the highscore for the new game
+              appdata[i].score = jsonInput.score;
+              if(newGameCurrentHS !== -1) { // There is a current highscore
+                let newGameUpdateHS = findHighscore(jsonInput.game);
+                if(i === newGameUpdateHS) { // New highscore
+                  appdata[i].highscore = true;
+                  appdata[newGameCurrentHS].highscore = false;
+                }
+              } else { // There isn't a current highscore
+                appdata[i].highscore = true;
+              }
+
+            }
+
+            console.log("Modification Complete");
+
+            break;  // Stop search
           }
         }
-      }
-      
-      /*let high = -1;
-      for(let i = 0; i < appdata.length; i++) { // Highscore Update
-        if(appdata[i].game === jsonInput.game && appdata[i].score < jsonInput.score) {
-          high = i;
-          appdata.highscore = false;
-          jsonInput.highscore = true;
-        } else if(appdata[i].game === jsonInput.game && appdata[i].score > jsonInput.score) {
-          jsonInput.highscore = false;
-          break;
-        }
-      }
-      if(high === -1) {
-        jsonInput.highscore = true;
-      }*/
-      
+      }      
     }
 
     response.writeHead( 200, "OK", {'Content-Type': 'text/plain' });
