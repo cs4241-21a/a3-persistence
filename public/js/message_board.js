0260184e52038b28becb4db708f1a6b21649f1c8
patrick-lee22 <prlee@wpi.edu>
2021-09-26 17:55:57 -0400

    Functionality fully works, just need to make it not look like ass

--- public/js/message_board.js
+++ public/js/message_board.js
@@ -4,9 +4,22 @@ const submit = function (e) {
 
     const nameText = document.getElementById('name');
     const targetText = document.getElementById('target-text');
+    const fancyFont = document.getElementById('fancy-font');
+    const textFaces = document.getElementById('text-faces');
+    const emojis = document.getElementById('emojis');
+    let checkedRadio = -1;
+    if(textFaces.checked) {
+        checkedRadio = 0;
+    } else if(emojis.checked) {
+        checkedRadio = 1;
+    } else {
+        checkedRadio = 2;
+    }
     json = {
         name: nameText.value,
-        message: targetText.value
+        message: targetText.value,
+        fancyFont: fancyFont.checked,
+        checkedRadio: checkedRadio
     };
     body = JSON.stringify(json)
 
@@ -18,7 +31,11 @@ const submit = function (e) {
         .then(function (response) {
             // do something with the reponse 
             response.text().then(function (str) {
-                addNewElt(JSON.parse(str));
+                let jsonObj = JSON.parse(str);
+                if(jsonObj.loggedInUser === jsonObj.obj.to) {
+                    addNewElt(jsonObj.obj);
+                }
+                addSentElement(jsonObj.obj);
             })
         })
 
@@ -29,9 +46,15 @@ const switchView = function (e) {
     if (e.target.id === 'table-nav') {
         document.getElementById('table-root').style.display = 'flex';
         document.getElementById('sentence-root').style.display = 'none';
-    } else {
+        document.getElementById('sent-root').style.display = 'none';
+    } else if (e.target.id === 'sentence-nav') {
         document.getElementById('table-root').style.display = 'none';
         document.getElementById('sentence-root').style.display = 'flex';
+        document.getElementById('sent-root').style.display = 'none';
+    } else {
+        document.getElementById('table-root').style.display = 'none';
+        document.getElementById('sentence-root').style.display = 'none';
+        document.getElementById('sent-root').style.display = 'flex';
     }
 }
 
@@ -42,27 +65,142 @@ function addNewElt(element) {
     let owoSentenceRoot = document.getElementById('owo-sentence-root');
 
     let normalrow = normalTable.insertRow(-1);
-    normalrow.insertCell(0).innerHTML = element.name;
-    normalrow.insertCell(1).innerHTML = element.message;
+    normalrow.insertCell(0).innerHTML = element.from;
+    let messagecell = normalrow.insertCell(1);
+    messagecell.innerHTML = element.message;
     let oworow = owoTable.insertRow(-1);
-    oworow.insertCell(0).innerHTML = element.nameowo;
-    oworow.insertCell(1).innerHTML = element.messageowo;
+    oworow.insertCell(0).innerHTML = element.fromowo;
+    let messageowocell = oworow.insertCell(1);
+    messageowocell.innerHTML = element.messageowo;
     let normalSentence = document.createElement('p');
-    normalSentence.innerHTML = `${element.name} said: "${element.message}"`;
+    normalSentence.innerHTML = `${element.from} said: "${element.message}"`;
     let owoSentence = document.createElement('p');
-    owoSentence.innerHTML = `${element.nameowo} said: "${element.messageowo}"`;
+    owoSentence.innerHTML = `${element.fromowo} said: "${element.messageowo}"`;
+    if(element.fancyFont) {
+        messagecell.className = 'fancy-font';
+        messageowocell.className = 'fancy-font';
+        normalSentence.className = 'fancy-font';
+        owoSentence.className = 'fancy-font';
+    }
     normalSentenceRoot.append(normalSentence);
     owoSentenceRoot.append(owoSentence);
 }
 
-window.onload = function () {
-    const button = document.querySelector('button')
-    button.onclick = submit;
-    const tableLink = document.getElementById('table-nav')
-    const sentenceLink = document.getElementById('sentence-nav')
-    tableLink.onclick = switchView;
-    sentenceLink.onclick = switchView;
-    fetch('/getAppdata', {
+function addSentElement(element, idx) {
+    const editRoot = document.getElementById('editable-root');
+    let newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+    <label for='message'>Message</label>
+    <textarea name='message' id='message${idx}'>${element.message}</textarea>
+    <label for='fancy-font'>Fancy Font?</label>
+    <input type="checkbox" id='fancy-font${idx}'>
+    <label for='text-faces${idx}'>Replace '!' With Text Faces</label>
+    <input type='radio' name='facereplace${idx}' id='text-faces${idx}'>
+    <label for='emojis${idx}'>Replace '!' With Emojis</label>
+    <input type='radio' name='facereplace${idx}' id='emojis${idx}'>
+    <label for='no-replace${idx}'>Do Not Replace '!'</label>
+    <input type='radio' name='facereplace${idx}' id='no-replace${idx}'>
+    <button class='inline-update-btn' id='inline-update-btn${idx}' data-index=${idx} data-dbid=${element._id}>Update</button>
+    <button class='inline-delete-btn' id='inline-delete-btn${idx}' data-index=${idx} data-dbid=${element._id}>Delete</button>`;
+    editRoot.append(newDiv);
+    switch(element.oworeplace) {
+        case 0:
+            document.getElementById('text-faces'+idx).checked = true;
+            break;
+        case 1:
+            document.getElementById('emojis'+idx).checked = true;
+            break;
+        case 2:
+            document.getElementById('no-replace'+idx).checked = true;
+            break;
+    }
+    if(element.fancyFont) {
+        document.getElementById('fancy-font'+idx).checked = true;
+    }
+
+    const updateBtn = document.getElementById('inline-update-btn'+idx);
+    const deleteBtn = document.getElementById('inline-delete-btn'+idx);
+
+    let updateFunc = function(e) {
+        e.preventDefault;
+        let btn = e.srcElement
+        let index = btn.getAttribute('data-index');
+        let dbid = btn.getAttribute('data-dbid');
+        let checkedRadio = -1;
+        if(document.getElementById('text-faces'+index).checked) {
+            checkedRadio = 0;
+        } else if (document.getElementById('emojis'+index).checked) {
+            checkedRadio = 1
+        } else {
+            checkedRadio = 2;
+        }
+        let obj = {
+            dbid: dbid,
+            message: document.getElementById('message'+index).value,
+            fancyFont: document.getElementById('fancy-font'+index).checked,
+            checkedRadio: checkedRadio
+        }
+        let body = JSON.stringify(obj);
+        fetch('/update', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body
+        }).then(function(response) {
+            clearAllMessages();
+            fetchAllMessages();
+        });
+    }
+    updateBtn.onclick = updateFunc;
+    let deleteFunc = function(e) {
+        e.preventDefault;
+        let btn = e.srcElement
+        let dbid = btn.getAttribute('data-dbid');
+        let obj = {
+            dbid: dbid
+        }
+        let body = JSON.stringify(obj);
+        fetch('/delete', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body
+        }).then(function(response) {
+            clearAllMessages();
+            fetchAllMessages();
+        });
+    }
+    deleteBtn.onclick = deleteFunc;
+    
+}
+
+function clearAllMessages() {
+    const normalTable = document.getElementById('normal-table');
+    const owoTable = document.getElementById('owo-table');
+    const normalSentence = document.getElementById('normal-sentence-root');
+    const owoSentence = document.getElementById('owo-sentence-root');
+    const sentMessages = document.getElementById('editable-root')
+
+    normalTable.innerHTML = 
+    `<thead>
+        <tr>
+        <th>Name</th>
+        <th>Message</th>
+        </tr>
+    </thead>`;
+    owoTable.innerHTML = 
+    `<thead>
+        <tr>
+        <th>Name</th>
+        <th>Message</th>
+        </tr>
+    </thead>`;
+    normalSentence.innerHTML = '<b>Normal Sentences</b>';
+    owoSentence.innerHTML = '<b>OwO-ified Sentences</b>';
+    sentMessages.innerHTML = '<b>Sent Messages</b>';
+  
+}
+
+function fetchAllMessages() {
+    fetch('/getMessagesTo', {
         method: 'GET',
     }).then(function (response) {
         response.text().then(function (jsonData) {
@@ -71,6 +209,32 @@ window.onload = function () {
             if (appdata.length !== 0) {
                 appdata.forEach(element => addNewElt(element));
             }
-        })
+        });
+    });
+
+    fetch('/getMessagesFrom', {
+        metod: 'GET',
+    }).then(function (response) {
+        response.text().then(function(jsonData) {
+            console.log(jsonData);
+            let appdata = JSON.parse(jsonData);
+            if(appdata.length !== 0) {
+                appdata.forEach(function(elt,idx) {
+                    addSentElement(elt,idx);
+                });
+            }
+        });
     })
+}
+
+window.onload = function () {
+    const button = document.getElementById('form-submit-btn')
+    button.onclick = submit;
+    const tableLink = document.getElementById('table-nav');
+    const sentenceLink = document.getElementById('sentence-nav');
+    const sentLink = document.getElementById('sent-nav');
+    tableLink.onclick = switchView;
+    sentenceLink.onclick = switchView;
+    sentLink.onclick = switchView;
+    fetchAllMessages();
 }
--- server_ex.js
+++ server_ex.js
@@ -1,90 +1,191 @@
 const express = require('express'),
-      mongodb = require('mongodb'),
-      serveStatic = require('serve-static'),
-      bodyParser = require('body-parser')
-      app = express(),
-      cookie  = require( 'cookie-session' ),
-      dir  = 'public/',
-      port = 3000
+  mongodb = require('mongodb'),
+  serveStatic = require('serve-static'),
+  bodyParser = require('body-parser')
+app = express(),
+  cookie = require('cookie-session'),
+  dir = 'public/',
+  port = 3000
+ObjectID = require('mongodb').ObjectID
 
 require('dotenv').config();
 
-const appdata = [{"name":"admin","message":"this is dummy appdata","nameowo":owoify("admin"),"messageowo":owoify("this is dummy appdata")}];
-const faces = ["(・`ω´・)",";;w;;","owo","UwU",">w<","^w^","(･.◤)","^̮^","(>人<)","( ﾟヮﾟ)","(▰˘◡˘▰)"]
+const faces = ["(・`ω´・)", ";;w;;", "owo", "UwU", ">w<", "^w^", "(･.◤)", "^̮^", "(>人<)", "( ﾟヮﾟ)", "(▰˘◡˘▰)"];
+const emojis = ["❤", "🤣", "😍", "😜", "😝", "😬", "🤪", "😳"];
 
-const uri = 'mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@'+process.env.DB_HOST
+const uri = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@' + process.env.DB_HOST
 
-const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
-let collection = null
+const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+let db = null;
 
 client.connect()
-  .then( () => {
+  .then(() => {
     // will only create collection if it doesn't exist
-    return client.db( 'assignment_3' ).collection( 'appdata' )
+    return client.db('assignment_3')
   })
-  .then( __collection => {
+  .then(__db => {
     // store reference to collection
-    collection = __collection
-    // blank query returns all documents
-    return collection.find({ }).toArray()
+    db = __db
   })
-  .then( console.log )
 
 
 app.use(serveStatic('public'))
-app.use( express.urlencoded({ extended:true }) )
+app.use(express.urlencoded({ extended: true }))
 
-app.use( cookie({
+app.use(cookie({
   name: 'session',
-  keys: ['key1', 'key2']
+  keys: ['Lucce', 'Janey']
 }))
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(`${__dirname}/public/login.html`)
 });
 
-app.post('/login', function(req, res) {
-  console.log( req.body )
-  
-  if( req.body.pass === 'test' ) {
-    console.log('test pw')
-    req.session.login = true
-    res.redirect( `/message_board.html` )
-  }else{
+app.post('/login', async function (req, res) {
+  let name = req.body.name;
+  let pass = req.body.pass;
+  let newUser = false;
+
+  let result = await db.collection('users').findOne({ _id: name });
+
+  if (!result) {
+    let obj = {
+      _id: name,
+      pass: pass
+    }
+
+    let firstRecord = {
+      "from": "God",
+      "to": name,
+      "fancyFont": false,
+      "oworeplace": 0,
+      "message": "Hello! Welcome to the message board!",
+      "fromowo": owoify("God", 0),
+      "messageowo": owoify("Hello! Welcome to the message board!", 0)
+    }
+
+    appdata = obj.records;
+    db.collection("users").insertOne(obj);
+    db.collection("records").insertOne(firstRecord)
+    newUser = true;
+    console.log("new user detected")
+  }
+
+  if (newUser || result.pass === pass) {
+    console.log("should be redirecting now")
+    req.session.login = true;
+    req.session.username = name;
+    res.redirect(`/message_board.html`)
+  } else {
     // password incorrect, redirect back to login page
-    res.sendFile( __dirname + '/public/login.html' )
+    res.sendFile(__dirname + '/public/login_failed.html')
   }
 });
 
-app.use( function( req,res,next) {
-  if( req.session.login === true )
+app.use(function (req, res, next) {
+  if (req.session.login === true)
     next()
   else
-    res.sendFile( __dirname + '/public/login.html' )
+    res.sendFile(__dirname + '/public/login.html')
 });
 
-app.get('/getAppdata', function(req, res) {
-  console.log("getappdata req")
-  res.send(JSON.stringify(appdata))
+app.get('/getMessagesTo', async function (req, res) {
+  let query = { to: req.session.username };
+  let appdata = [];
+  let results = await db.collection("records").find(query);
+  results.forEach(function (elt, idx) {
+    appdata.push(elt);
+  }).then(() => {
+    res.send(JSON.stringify(appdata))
+  })
+
 });
 
-app.post('/submit', bodyParser.json(), function(req, res) {
+app.get('/getMessagesFrom', async function (req, res) {
+  let query = { from: req.session.username };
+  let appdata = [];
+  let results = await db.collection("records").find(query);
+  results.forEach(function (elt, idx) {
+    appdata.push(elt);
+  }).then(() => {
+    res.send(JSON.stringify(appdata))
+  })
+})
+
+app.post('/submit', bodyParser.json(), async function (req, res) {
   let obj = {
-    name: req.body.name,
+    from: req.session.username,
+    to: req.body.name,
+    fancyFont: req.body.fancyFont,
+    oworeplace: req.body.checkedRadio,
     message: req.body.message,
-    nameowo: owoify(req.body.name),
-    messageowo: owoify(req.body.message)
+    fromowo: owoify(req.session.username, req.body.checkedRadio),
+    messageowo: owoify(req.body.message, req.body.checkedRadio)
   }
-  appdata.push(obj);
-  res.send(JSON.stringify(obj))
+  await db.collection('records').insertOne(obj);
+
+  res.send(JSON.stringify({
+    obj: obj,
+    loggedInUser: req.session.username
+  }));
 });
 
-function owoify(text) {
-  //console.log(text)
+app.post('/update', bodyParser.json(), async function (req, res) {
+  console.log("logging update request: " + req.body.dbid);
+  const query = {
+    _id: ObjectID(req.body.dbid)
+  }
+  const resultSet = await db.collection('records').findOne(query);
+  let updateFields;
+  if (resultSet.message === req.body.message && resultSet.oworeplace === req.body.checkedRadio) {
+    updateFields = {
+      $set: {
+        fancyFont: req.body.fancyFont
+      }
+    }
+  } else {
+    updateFields = {
+      $set: {
+        fancyFont: req.body.fancyFont,
+        oworeplace: req.body.checkedRadio,
+        message: req.body.message,
+        messageowo: owoify(req.body.message, req.body.checkedRadio)
+      }
+    }
+  }
+
+  const updateResults = await db.collection('records').updateOne(query, updateFields);
+  console.log("Number updated: " + updateResults.modifiedCount)
+
+  res.send("It worked");
+});
+
+app.post('/delete', bodyParser.json(), async function (req, res) {
+  console.log("logging delete request: " + req.body.dbid);
+  const query = {
+    _id: ObjectID(req.body.dbid)
+  }
+  const result = await db.collection('records').deleteOne(query);
+  console.log("Number deleted: " + result.deletedCount);
+  res.send("wahoo");
+});
+
+function owoify(text, expReplace) {
   let v = text.replace(/[lr]/g, 'w').replace(/[LR]/g, 'W').replace(/n[aeiou]/g, 'ny').replace(/N[aeiou]/g, 'Ny').replace(/N[AEIOU]/g, 'NY');
-  let numExclaimations = (v.match(/!/g)||[]).length;
-  for(let i = 0; i < numExclaimations; i++) {
-    v = v.replace('!'," " + faces[getRandomInt(0, faces.length)] + " ");
+  let numExclaimations = 0;
+  switch (expReplace) {
+    case 0: // text faces
+      numExclaimations = (v.match(/!/g) || []).length;
+      for (let i = 0; i < numExclaimations; i++) {
+        v = v.replace('!', " " + faces[getRandomInt(0, faces.length)] + " ");
+      }
+      break;
+    case 1: // emojis
+      numExclaimations = (v.match(/!/g) || []).length;
+      for (let i = 0; i < numExclaimations; i++) {
+        v = v.replace('!', " " + emojis[getRandomInt(0, emojis.length)] + " ");
+      }
+      break;
   }
   return v;
 }
