89b14908fc0c4ca36fb92ac91782e512d34e8313
David M <PieKing1215@users.noreply.github.com>
2021-09-27 01:47:24 -0400

    Fix github auth callbackURL

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ passport.deserializeUser(function(user, cb) {
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "http://localhost:3001/auth/github/callback"
+    callbackURL: "https://a3-david-mahany.glitch.me/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     cb(null, profile);
