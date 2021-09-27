4049a1dd49fcc6c9d55d6bcd16330db243d96b46
lewasserman <laurenewasserman@gmail.com>
2021-09-27 06:34:45 -0400

    Working on DB

--- public/script.js
+++ public/script.js
@@ -1,41 +1,87 @@
 // client-side js, loaded by index.html
-// run by the browser each time the page is loaded
-
-console.log("hello world :o");
 
 // define variables that reference elements on our page
-const dreamsList = document.getElementById("dreams");
-const dreamsForm = document.querySelector("form");
+const booksTable = document.getElementById("books");
+const booksForm = document.querySelector("form");
+// const dreamsList = document.getElementById("dreams");
+// const dreamsForm = document.querySelector("form");
 
-// a helper function that creates a list item for a given dream
-function appendNewDream(dream) {
-  const newListItem = document.createElement("li");
-  newListItem.innerText = dream;
-  dreamsList.appendChild(newListItem);
+// a helper function that creates a table item for a given dream
+function appendNewBook(book) {
+  const newTableItem = document.createElement("tr");
+  var tableText = '<tr><th>Title</th><th>Author</th><th>Release Year</th><th>Rating</th><th>Rank</th></tr>'
+  tableText +='<tr><td>'
+  tableText += book[0]
+  tableText += '</td><td>'
+  tableText += book[1]
+  tableText += '</td><td>'
+  tableText += book[2]
+  tableText += '</td><td>'
+  tableText += book[3]
+  tableText += '</td><td>'
+  tableText += book[4]
+  tableText += '</td></tr>'
+  newTableItem.innerHTML = tableText
+  // document.querySelector('tr').remove()
+  // document.body.appendChild(newTableItem)
+  booksTable.append(newTableItem)
 }
+// function appendNewDream(dream) {
+//   const newListItem = document.createElement("li");
+//   newListItem.innerText = dream;
+//   dreamsList.appendChild(newListItem);
+// }
 
-// fetch the initial list of dreams
-fetch("/dreams")
+// fetch the initial list of books
+fetch("/books")
   .then(response => response.json()) // parse the JSON from the server
-  .then(dreams => {
+  .then(books => {
     // remove the loading text
-    dreamsList.firstElementChild.remove();
+    // booksTable.firstElementChild.remove();
   
-    // iterate through every dream and add it to our page
-    dreams.forEach(appendNewDream);
+    // iterate through every book and add it to our page
+    // books.forEach(appendNewBook);
   
-    // listen for the form to be submitted and add a new dream when it is
-    dreamsForm.addEventListener("submit", event => {
+    // listen for the form to be submitted and add a new book when it is
+    booksForm.addEventListener("submit", event => {
       // stop our form submission from refreshing the page
       event.preventDefault();
 
-      // get dream value and add it to the list
-      let newDream = dreamsForm.elements.dream.value;
-      dreams.push(newDream);
-      appendNewDream(newDream);
+      // get book value and add it to the table
+      let newBook = [booksForm.elements["title"],
+        booksForm.elements["author"],
+        booksForm.elements["year"],
+        booksForm.elements["rating"],
+        booksForm.elements["rank"]];
+      books.push(newBook);
+      appendNewBook(newBook);
 
       // reset form
-      dreamsForm.reset();
-      dreamsForm.elements.dream.focus();
+      booksForm.reset();
+      // booksForm.elements.book.focus();
     });
   });
+// fetch("/dreams")
+//   .then(response => response.json()) // parse the JSON from the server
+//   .then(dreams => {
+//     // remove the loading text
+//     dreamsList.firstElementChild.remove();
+  
+//     // iterate through every dream and add it to our page
+//     dreams.forEach(appendNewDream);
+  
+//     // listen for the form to be submitted and add a new dream when it is
+//     dreamsForm.addEventListener("submit", event => {
+//       // stop our form submission from refreshing the page
+//       event.preventDefault();
+
+//       // get dream value and add it to the list
+//       let newDream = dreamsForm.elements.dream.value;
+//       dreams.push(newDream);
+//       appendNewDream(newDream);
+
+//       // reset form
+//       dreamsForm.reset();
+//       dreamsForm.elements.dream.focus();
+//     });
+//   });
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 // server.js
 // where your node app starts
 
+require('dotenv').config()
+
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 // const express = require("express");
 // const app = express();
 const express    = require('express'),
-      app        = express(),
+      mongodb = require( 'mongodb' ),
+      refleftmd = require( 'reflect-metadata' ),
       bodyparser = require( 'body-parser' ),
+      app        = express(),
       port = 3000,
       mime = require( 'mime' )
       // dreams     = []
@@ -26,7 +30,54 @@ var appdata = []
 app.use(express.static("public"));
 
 // get json when appropriate
-app.use( bodyparser.json() )
+app.use( express.json() )
+
+const uri = 'mongodb://'+process.env.USER+':'+process.env.PASS+'@'+process.env.HOST
+
+const client = new mongodb.MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology:true })
+let collection = null
+
+client.connect()
+  .then( () => {
+    // will only create collection if it doesn't exist
+    return client.db( 'RateYourBooks' ).collection( 'Books' )
+  })
+  .then( __collection => {
+    // store reference to collection
+    collection = __collection
+    // blank query returns all documents
+    return collection.find({ }).toArray()
+  })
+  .then( console.log )
+
+app.use( (req,res,next) => {
+  if( collection !== null ) {
+    next()
+  }else{
+    res.status( 503 ).send()
+  }
+})
+
+app.post( '/add', (req,res) => {
+  // assumes only one object to insert
+  collection.insertOne( req.body ).then( result => res.json( result ) )
+})
+
+// assumes req.body takes form { _id:5d91fb30f3f81b282d7be0dd } etc.
+app.post( '/remove', (req,res) => {
+  collection
+    .deleteOne({ _id:mongodb.ObjectId( req.body._id ) })
+    .then( result => res.json( result ) )
+})
+
+app.post( '/update', (req,res) => {
+  collection
+    .updateOne(
+      { _id:mongodb.ObjectId( req.body._id ) },
+      { $set:{ name:req.body.name } }
+    )
+    .then( result => res.json( result ) )
+})
 
 // https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
@@ -79,9 +130,9 @@ app.post( '/submit-book', bodyparser.json(), function( request, response ) {
 })
 
 // send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
+app.get("/books", (request, response) => {
   // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+  response.json(appdata);
 });
 
 // listen for requests :)
