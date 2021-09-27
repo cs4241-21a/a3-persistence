3793fdde7db3cfd12675fe14179cfb06b84b0da6
Glitch (a3-msacristanbenjet) <none>
2021-09-27 14:12:40 +0000

    A3- MsacristanBenjet

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const data = require("/public/data")
 const morgan = require('morgan')
-//const favicon = require('serve-favicon')
+const favicon = require('serve-favicon')
 const cors = require('cors')
 
 
