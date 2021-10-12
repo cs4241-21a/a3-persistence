dc6294891e59a42532b5215c50cb0af3d4c9cf9e
fedeit <fgalbiati@wpi.edu>
2021-10-11 23:49:55 -0400

    Update passport-auth.js

--- services/passport-auth.js
+++ services/passport-auth.js
@@ -1,7 +1,9 @@
 module.exports = (req, res, next) => {
-    if (req.user && req.user.emails && req.user.emails[0]) {
-      next()
-    } else {
-      res.redirect('/auth/login')
-    }
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+  console.log(`User ${ip} is logged in: ${req.isAuthenticated()}`)
+  if (req.isAuthenticated()) {
+      console.log(`User ${ip}: username: ${req.user.username}`)
+      return next();
+  }
+  res.redirect('/auth/login')
 }
